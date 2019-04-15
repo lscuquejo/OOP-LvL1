3dd2527d7a227d57267547a82c74982dc0dccbbe
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-04-15 12:03:03 +0100

    8th ep

--- OOP/lib/Ship.php
+++ OOP/lib/Ship.php
@@ -20,7 +20,7 @@ class Ship
         return $this->name;
     }
 
-    public function getNameAndSpecs($useShortFormat)
+    public function getNameAndSpecs($useShortFormat = false)
     {
         if ($useShortFormat){
             return sprintf(
@@ -45,41 +45,4 @@ class Ship
     {
         return $givenShip->strength > $this->strength;
     }
-}
-
-/**
- * @param Ship $someShip
- */
-function printShipSummary($someShip)
-{
-    echo 'Ship name'.$someShip->name;
-    echo '<hr/>';
-    $someShip->sayHello();
-    echo '<hr/>';
-    echo $someShip->getName();
-    echo '<hr/>';
-    echo $someShip->getNameAndSpecs(false);
-    echo '<hr/>';
-    echo $someShip->getNameAndSpecs(true);
-}
-
-$myShip = new Ship();
-$myShip->name = 'Jedi Starship';
-$myShip->weaponPower = 10;
-
-$otherShip = new Ship();
-$otherShip->name = 'Imperial Shuttle';
-$otherShip->weaponPower = 5;
-$otherShip->strength = 50;
-
-
-printShipSummary($myShip);
-echo "<br>"."<br>"."<br>";
-printShipSummary($otherShip);
-
-echo '<hr/>';
-if ($myShip->doesGivenShipHaveMoreStrength($otherShip)) {
-    echo $otherShip->name.' has more strength';
-} else {
-    echo $myShip->name.' has more strength';
 }
