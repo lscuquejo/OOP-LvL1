72dae5aa4c6da75a655f895a2e3cf34b84647c8b
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-04-29 10:51:24 +0100

    OOP Part 2 (10th ep)

---
+++ OOP-Course-2/eps/ep10/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/lib/Ship.php';
+require_once __DIR__ . '/lib/BattleManager.php';
+require_once __DIR__ . '/lib/ShipLoader.php';
+require_once __DIR__ . '/lib/BattleResult.php';
+
+$configuration = array(
+    'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => '123mudar',
+);
