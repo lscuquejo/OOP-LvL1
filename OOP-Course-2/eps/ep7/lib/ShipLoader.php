0e628d8f7898e36ad8a6cef10056eda147972e4c
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-04-29 10:20:00 +0100

    OOP Part 2 (7th ep)

---
+++ OOP-Course-2/eps/ep7/lib/ShipLoader.php
@@ -0,0 +1,59 @@
+<?php
+class ShipLoader
+{
+    /**
+     * @return Ship[]
+     */
+    public function getShips()
+    {
+        $ships = array();
+
+        $shipsData = $this->queryForShips();
+
+        foreach ($shipsData as $shipData) {
+            $ships[] = $this->createShipFromData($shipData);
+        }
+
+        return $ships;
+    }
+
+    /**
+     * @param $id
+     * @return Ship|null
+     */
+    public function findOneById($id)
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root', '123mudar');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('SELECT * FROM ship WHERE id = :id');
+        $statement->execute(array('id' => $id));
+        $shipArray = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if (!$shipArray) {
+            return null;
+        }
+
+        return $this->createShipFromData($shipArray);
+    }
+
+    private function createShipFromData(array $shipData)
+    {
+        $ship = new Ship($shipData['name']);
+        $ship->setId($shipData['id']);
+        $ship->setWeaponPower($shipData['weapon_power']);
+        $ship->setJediFactor($shipData['jedi_factor']);
+        $ship->setStrength($shipData['strength']);
+
+        return $ship;
+    }
+
+    private function queryForShips()
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root', '123mudar');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('SELECT * FROM ship');
+        $statement->execute();
+        $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $shipsArray;
+    }
+}
