acdcc5cd853e39e466f9fe31df590e796a4cad8a
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-05-17 10:19:34 +0100

    OOP-3 ep3

---
+++ OOP-Course-3/eps/ep3/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => '123mudar',
+);
+
+require_once __DIR__.'/lib/Service/Container.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Model/RebelShip.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
